72411dae346d3eb348e5e2d74f3bf7d7636a3a42
Takala <dylan91011028@gmail.com>
2026-01-08 23:01:11 +0800

    refactor: remove prettier configuration from ESLint setup

--- packages/mentora-api/eslint.config.js
+++ packages/mentora-api/eslint.config.js
@@ -1,10 +1,9 @@
-import prettier from 'eslint-config-prettier';
-import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
+import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
@@ -15,8 +14,6 @@ export default defineConfig(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 
