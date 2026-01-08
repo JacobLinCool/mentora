10c5d5547f44c1f14890c6548185272109719f77
Takala <dylan91011028@gmail.com>
2026-01-08 23:07:36 +0800

    refactor: reorganize ESLint configuration and integrate Prettier support

--- packages/mentora-api/eslint.config.js
+++ packages/mentora-api/eslint.config.js
@@ -1,9 +1,10 @@
+import prettier from 'eslint-config-prettier';
+import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
@@ -14,6 +15,8 @@ export default defineConfig(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs.recommended,
+	prettier,
+	...svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 
